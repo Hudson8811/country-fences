ad72a0015dcca6ee2295158cc14c09c63fe57bb1
Hudson8811 <hudson88@yandex.ru>
2019-03-16 20:52:32 +0400

    type page

--- js/script.js
+++ js/script.js
@@ -257,3 +257,45 @@ if (window.innerWidth < 651) {
       $('.palette_slider').slick('slickFilter','.big_slide');
     }
 };
+
+$('.slide4resp').owlCarousel({
+    loop:true,
+    margin:20,
+	nav:true,
+	navText:false,
+	dots:true,
+	responsive:{
+		0:{
+            items:1,
+            nav:true
+        },
+		500:{
+            items:2,
+            nav:true
+        },
+		
+		800:{
+            items:3,
+            nav:true
+        },
+		1200:{
+            items:4,
+            nav:true
+        }
+    }
+});
+
+$('.slide4resp2').owlCarousel({
+    loop:true,
+    margin:20,
+	nav:true,
+	navText:false,
+	dots:true,
+	responsive:{
+
+		1200:{
+            items:1,
+            nav:true
+        }
+    }
+});
